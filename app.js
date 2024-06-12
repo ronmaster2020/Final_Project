6173e4e5b607eddfef77d2d0c5123d510d954d31
Ron Ochakovsky <ron.ochak@gmail.com>
2024-06-12 14:45:58 +0300

    MVC architecure + node js server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
