83b84d1df232a1b094992ac3fad9a9ad48952cc9
Ron Ochakovsky <ron.ochak@gmail.com>
2024-06-14 13:00:44 +0300

    path package

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
