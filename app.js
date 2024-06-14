67dac450ba7de9a1cd6ed09728ae6721b83bd2c9
David Sheylar <david.sheylar@gmail.com>
2024-06-14 14:08:13 +0300

    add cart

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
@@ -12,3 +14,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+app.post('/cart/add', (req, res) => {
+    const inputValue = req.body.product_id; // Replace 'inputName' with the name of your input field
+    console.log(inputValue); // Print the value to the console  
+    res.sendStatus(200);
+})
