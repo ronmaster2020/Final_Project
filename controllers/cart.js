adc3d9df59bde6215e0c074d3374c0542dec2bc1
David Sheylar <david.sheylar@gmail.com>
2024-06-14 17:33:12 +0300

    cart add

--- controllers/cart.js
+++ controllers/cart.js
@@ -7,5 +7,16 @@ exports.AddToCart = async(req, res) => {
         return res.status(503).send('Service unavailable. Please try again later.');
     }
 
-    
+    try {
+        const cart = new Cart({
+            customer_id: req.body.customer_id,
+            //product_id: req.body.product_id
+        });
+        await cart.save();
+        res.send(cart.customer_id + ' was added to cart successfully');
+    } catch (err) {
+        console.error('Error adding product to cart:', err);
+        res.status(500).send('Server error');
+    }
+
 }
