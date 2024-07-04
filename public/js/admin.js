a2977da76a9a228b7501992d05a7eb3180808828
Ron Ochakovsky <ron.ochak@gmail.com>
2024-07-04 17:24:00 +0300

    admin navbar bug fixes and added toggle button for medium screens

---
+++ public/js/admin.js
@@ -0,0 +1,26 @@
+var sideNavbar = true;
+
+$('document').ready(function() {
+    addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            $('nav').removeClass('translateX-100');
+            sideNavbar = false;
+        } else {
+            $('nav').addClass('translateX-100');
+            sideNavbar = true;
+        }
+    });
+});
+
+function openSideNavbar() {
+    var nav = $('nav');
+
+    if (sideNavbar) {
+        nav.removeClass('translateX-100');
+        sideNavbar = false;
+
+    } else {
+        nav.addClass('translateX-100');
+        sideNavbar = true;
+    }
+}
