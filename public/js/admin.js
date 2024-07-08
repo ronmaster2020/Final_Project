6aaec4e23549315c7f46fef14d5a0e8387ffb968
Ron Ochakovsky <ron.ochak@gmail.com>
2024-07-08 19:48:54 +0300

    skeleton of admin dashboard, and made admin' fetch data function with load indicator

--- public/js/admin.js
+++ public/js/admin.js
@@ -24,3 +24,37 @@ function openSideNavbar() {
         sideNavbar = true;
     }
 }
+
+async function fetchData(query, url, method, data_container) {
+    // initialize data container
+    data_container.empty();
+    // Start loading indicator
+    let loadingText = 'loading';
+    const loadingIndicator = $('#loadingIndicator');
+    const loadTxt = $('#loadTxt');
+    loadTxt.text(loadingText);
+
+    loadingIndicator.removeClass('d-none');
+    data_container.addClass('d-none');
+
+    const interval = setInterval(() => {
+        loadingText = loadingText.length < 10 ? loadingText + '.' : 'loading';
+        loadTxt.text(loadingText);
+    }, 500);
+    
+    // get products data from the server route
+    const queryParams = new URLSearchParams(query).toString();
+    const response = await fetch(`${url}?${queryParams}`, {
+        method: method, // GET request
+        headers: {
+            'Content-Type': 'application/json', // Assuming the server expects JSON
+        },
+    });
+
+    // Stop loading indicator
+    clearInterval(interval);
+    loadingIndicator.addClass('d-none');
+    data_container.removeClass('d-none');
+
+    return await response.json();
+}
