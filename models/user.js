9e9d9eed8bc884d7ce34322d43a4ae27160a8b84
Ron Ochakovsky <ron.ochak@gmail.com>
2024-07-27 14:28:01 +0300

    added googleId field to user model

--- models/user.js
+++ models/user.js
@@ -54,6 +54,10 @@ const userSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'cart',
+    },
+    googleId: {
+        type: String, 
+        unique: true 
     }
 });
 
