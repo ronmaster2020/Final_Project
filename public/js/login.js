75ddcdb169babef19729d74371c0968372cf85fa
Ron Ochakovsky <ron.ochak@gmail.com>
2024-07-28 14:59:45 +0300

    fixed a bug

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
-
+    
     loginForm.addEventListener('submit', async (event) => {
         event.preventDefault();
-
+    
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
-
+    
         try {
             const response = await fetch('/login', {
                 method: 'POST',
@@ -15,33 +15,31 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify({ email, password })
             });
-
-            if (response.ok) {
-                const data = await response.json();
-                localStorage.setItem('token', data.token);
-
-                // Redirect to the user page or any other page
-                window.location.href = '/userPage.html';
-            } else {
+    
+            if (!response.ok) {
                 const errorData = await response.json();
                 showToast(errorData.message || 'Incorrect email or password');
+            } else {
+                // Follow the redirection URL provided by the server
+                const redirectUrl = response.url;
+                window.location.href = redirectUrl;
             }
         } catch (error) {
             console.error('Error during login:', error.message);
             showToast('Error during login.');
         }
     });
-
+    
     function showToast(message) {
         const toast = document.createElement('div');
         toast.className = 'toast';
         toast.innerText = message;
         document.body.appendChild(toast);
-
+    
         setTimeout(() => {
             toast.classList.add('show');
         }, 100);
-
+    
         setTimeout(() => {
             toast.classList.remove('show');
             setTimeout(() => {
